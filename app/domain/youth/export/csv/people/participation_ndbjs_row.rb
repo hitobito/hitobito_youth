b6c96c2c19961938b02169f504f347b9cb29e892
pascal simon <simon@puzzle.ch>
2015-06-30 16:48:24 +0200

    wip: ndbjs csv export

---
+++ app/domain/youth/export/csv/people/participation_ndbjs_row.rb
@@ -0,0 +1,84 @@
+module Youth::Export::Csv::People
+  class ParticipationNdbjsRow < Export::Csv::People::PersonRow
+
+    attr_reader :participation
+
+    def initialize(participation)
+      @participation = participation
+      super(participation.person)
+    end
+
+    def gender
+      {'m': 1, 'f': 2}[entry.gender]
+    end
+
+    def ahv_number
+      '?'
+    end
+
+    def canton
+      '?'
+    end
+
+    def country
+      if entry.swiss?
+        'CH'
+      end
+    end
+
+    def phone_private
+      phone_number('Privat')
+    end
+
+    def phone_work
+      phone_number('Arbeit')
+    end
+
+    def phone_mobile
+      phone_number('Mobil')
+    end
+
+    def phone_fax
+      phone_number('Fax')
+    end
+
+    def nationality
+      '?'
+    end
+
+    def first_language
+      lang = entry.correspondence_language
+      lang.first.capitalize if lang.present?
+    end
+
+    def second_language
+      nil
+    end
+
+    def profession
+      3
+    end
+
+    def organisation
+      nil
+    end
+
+    def association
+      nil
+    end
+
+    def activity
+      1
+    end
+
+    def attachments
+      1
+    end
+
+    private
+    def phone_number(label)
+      entry.phone_numbers.find_by(label: label).try(:number)
+    end
+
+  end
+end
---
+++ app/domain/youth/export/csv/people/participations_ndbjs.rb
@@ -0,0 +1,38 @@
+module Youth::Export::Csv::People
+  class ParticipationsNdbjs < Export::Csv::Base
+
+    self.model_class = ::Participation
+    self.row_class = Export::Csv::People::ParticipationNdbjsRow
+
+    def build_attribute_labels
+      { j_s_number: 'NDS_PERSONEN_NR',
+        gender: 'GESCHLECHT',
+        last_name: 'NAME',
+        first_name: 'VORNAME',
+        birthday: 'GEBURTSDATUM',
+        ahv_number: 'AHV_NUMMER',
+        address: 'STREET',
+        zip_code: 'PLZ',
+        town: 'ORT',
+        canton: 'KANTON',
+        country: 'LAND',
+        phone_private: 'TELEFON_PRIVAT',
+        phone_work: 'TELEFON_GESCHAEFT',
+        phone_mobile: 'TELEFON_MOBIL',
+        phone_fax: 'FAX',
+        email: 'EMAIL',
+        nationality: 'NATIONALITAET',
+        first_language: 'ERSTSPRACHE',
+        second_language: 'ZWEITSPRACHE',
+        profession: 'BERUF',
+        organisation: 'VERPFLICHT_ORG',
+        association: 'VERPFLICHT_VERB',
+        activity: 'TAETIGKEIT',
+        attachments: 'BEILAGEN'
+      }
+    end
+
+    private
+
+  end
+end
