39063baf3d5d8701e730bce3ee32cf8983e83dad
amaierhofer <a.maierhofer@gmail.com>
2015-07-02 15:47:33 +0200

    adding migration for course related fields

---
+++ db/migrate/20150702133558_add_course_related_fields.rb
@@ -0,0 +1,39 @@
+class AddCourseRelatedFields < ActiveRecord::Migration
+  def up
+    add_if_missing(:events, :signature, :boolean)
+    add_if_missing(:events, :signature_confirmation, :boolean)
+
+    add_if_missing(:event_kinds, :bsv_id, :string)
+
+    add_if_missing(:people, :nationality, :string)
+    add_if_missing(:people, :ahv_number, :string)
+    add_if_missing(:people, :canton, :string)
+    add_if_missing(:people, :j_s_number, :string) # integer in cevi
+  end
+
+  def down
+    remove_if_present(:events, :signature)
+    remove_if_present(:events, :signature_confirmation)
+
+    remove_if_present(:event_kinds, :bsv_id)
+
+    remove_if_present(:people, :nationality)
+    remove_if_present(:people, :ahv_number)
+    remove_if_present(:people, :canton)
+    remove_if_present(:people, :j_s_number)
+  end
+
+  private
+
+  def add_if_missing(table, column, type, args = {})
+    if !column_exists?(table, column)
+      add_column(table, column, type, args)
+    end
+  end
+
+  def remove_if_present(table, column)
+    if column_exists?(table, column)
+      remove_column(table, column)
+    end
+  end
+end
