f84630fe0499f8f6e9410458231d9e4c16bda03f
Pascal Zumkehr <pascal@codez.ch>
2015-09-22 21:33:13 +0200

    group_full is not able to create tentative applications

--- app/abilities/youth/event/participation_ability.rb
+++ app/abilities/youth/event/participation_ability.rb
@@ -19,21 +19,10 @@ module Youth::Event::ParticipationAbility
       permission(:layer_and_below_full).may(:cancel, :reject, :absent, :assign, :attend).
         in_same_layer
 
-      permission(:group_full).may(:create_tentative).person_in_same_group
-      permission(:group_and_below_full).may(:create_tentative).person_in_same_group_or_below
       permission(:layer_full).may(:create_tentative).person_in_same_layer
       permission(:layer_and_below_full).may(:create_tentative).person_in_same_layer_or_visible_below
       general(:create_tentative).event_tentative_and_person_in_tentative_group
     end
-  end
-
-  def person_in_same_group
-    person.nil? || permission_in_groups?(person.groups.collect(&:id))
-  end
-
-  def person_in_same_group_or_below
-    person.nil? ||
-    permission_in_groups?(person.groups.collect(&:local_hierarchy).flatten.collect(&:id).uniq)
   end
 
   def person_in_same_layer
@@ -41,14 +30,14 @@ module Youth::Event::ParticipationAbility
   end
 
   def person_in_same_layer_or_visible_below
-    person.nil? || person_in_same_layer || visible_below
+    person_in_same_layer || visible_below
   end
 
   def event_tentative_and_person_in_tentative_group
     return false unless event.tentative_application_possible?
 
     groups = tentative_group_ids
-    permission_in_groups?(groups) || permission_in_layers?(groups)
+    permission_in_layers?(groups)
   end
 
   private
@@ -68,6 +57,5 @@ module Youth::Event::ParticipationAbility
   def tentative_group_ids
     event.groups.flat_map { |g| g.self_and_descendants.pluck(:id) + g.hierarchy.pluck(:id) }
   end
-
 
 end
