73843aa4f6bb4fe7fe3a80b341332b753b8257df
Matthias Viehweger <viehweger@puzzle.ch>
2021-06-16 00:31:14 +0200

    Conform to linter and styleguide

--- lib/hitobito_youth/wagon.rb
+++ lib/hitobito_youth/wagon.rb
@@ -1,4 +1,5 @@
 # encoding: utf-8
+# frozen_string_literal: true
 
 #  Copyright (c) 2012-2015, Pfadibewegung Schweiz. This file is part of
 #  hitobito_youth and licensed under the Affero General Public License version 3
@@ -13,22 +14,22 @@ module HitobitoYouth
     app_requirement '>= 0'
 
     # Add a load path for this specific wagon
-    config.autoload_paths += %W(
+    config.autoload_paths += %W[
       #{config.root}/app/abilities
       #{config.root}/app/domain
       #{config.root}/app/jobs
-    )
+    ]
 
-    config.to_prepare do
+    config.to_prepare do # rubocop:disable Metrics/BlockLength
       # extend application classes here
 
       # models
-      Person.send :include, Youth::Person
-      Event.send :include, Youth::Event
-      Event::Course.send :include, Youth::Event::Course
-      Event::Participation.send :include, Youth::Event::Participation
-      Event::ParticipationContactData.send :include, Youth::Event::ParticipationContactData
-      Event::Role.send :include, Youth::Event::Role
+      Person.include Youth::Person
+      Event.include Youth::Event
+      Event::Course.include Youth::Event::Course
+      Event::Participation.include Youth::Event::Participation
+      Event::ParticipationContactData.include Youth::Event::ParticipationContactData
+      Event::Role.include Youth::Event::Role
 
       TableDisplay::People.register_permission(Person,
                                                :show_full,
@@ -37,45 +38,45 @@ module HitobitoYouth
                                                :nationality_j_s)
 
       # domain
-      Event::ParticipationFilter.send :include, Youth::Event::ParticipationFilter
-      Event::ParticipantAssigner.send :include, Youth::Event::ParticipantAssigner
-      Export::Tabular::Events::List.send :include, Youth::Export::Tabular::Events::List
-      Export::Tabular::Events::Row.send :include, Youth::Export::Tabular::Events::Row
-      Person::AddRequest::Approver::Event.send :include, Youth::Person::AddRequest::Approver::Event
+      Event::ParticipationFilter.include Youth::Event::ParticipationFilter
+      Event::ParticipantAssigner.include Youth::Event::ParticipantAssigner
+      Export::Tabular::Events::List.include Youth::Export::Tabular::Events::List
+      Export::Tabular::Events::Row.include Youth::Export::Tabular::Events::Row
+      Person::AddRequest::Approver::Event.include Youth::Person::AddRequest::Approver::Event
 
       # ability
-      GroupAbility.send :include, Youth::GroupAbility
-      EventAbility.send :include, Youth::EventAbility
-      Event::ParticipationAbility.send :include, Youth::Event::ParticipationAbility
+      GroupAbility.include Youth::GroupAbility
+      EventAbility.include Youth::EventAbility
+      Event::ParticipationAbility.include Youth::Event::ParticipationAbility
 
       # decorator
-      Event::ParticipationDecorator.send :include, Youth::Event::ParticipationDecorator
+      Event::ParticipationDecorator.include Youth::Event::ParticipationDecorator
 
       # controller
-      PeopleFiltersController.send :include, Youth::PeopleFiltersController
-      Event::ParticipationsController.send :include, Youth::Event::ParticipationsController
+      PeopleFiltersController.include Youth::PeopleFiltersController
+      Event::ParticipationsController.include Youth::Event::ParticipationsController
 
       PeopleController.permitted_attrs += [:nationality_j_s, :ahv_number, :j_s_number]
       EventsController.permitted_attrs += [:tentative_applications]
       Event::KindsController.permitted_attrs += [:kurs_id_fiver, :vereinbarungs_id_fiver]
 
-      Event::ParticipationsController.send :include, Youth::Event::ParticipationsController
-      Event::ListsController.send :include, Youth::Event::ListsController
-      Event::RolesController.send :include, Youth::Event::RolesController
+      Event::ParticipationsController.include Youth::Event::ParticipationsController
+      Event::ListsController.include Youth::Event::ListsController
+      Event::RolesController.include Youth::Event::RolesController
 
       # job
       Export::EventParticipationsExportJob.
-        send :include, Youth::Export::EventParticipationsExportJob
+        include Youth::Export::EventParticipationsExportJob
 
       # helper
-      FilterNavigation::People.send :include, Youth::FilterNavigation::People
-      Sheet::Group.send :include, Youth::Sheet::Group
-      Sheet::Event.send :include, Youth::Sheet::Event
-      Dropdown::PeopleExport.send :include, Youth::Dropdown::PeopleExport
-      Dropdown::Event::ParticipantAdd.send :include, Youth::Dropdown::Event::ParticipantAdd
+      FilterNavigation::People.include Youth::FilterNavigation::People
+      Sheet::Group.include Youth::Sheet::Group
+      Sheet::Event.include Youth::Sheet::Event
+      Dropdown::PeopleExport.include Youth::Dropdown::PeopleExport
+      Dropdown::Event::ParticipantAdd.include Youth::Dropdown::Event::ParticipantAdd
 
       # serializer
-      PersonSerializer.send :include, Youth::PersonSerializer
+      PersonSerializer.include Youth::PersonSerializer
     end
 
     initializer 'youth.add_settings' do |_app|
