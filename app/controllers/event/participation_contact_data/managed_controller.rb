775c5cc006226a046981c7b1fd1d327d062ae2af
Nils Rauch <rauch@puzzle.ch>
2023-05-02 16:39:58 +0200

    Check for duplicates

--- app/controllers/event/participation_contact_data/managed_controller.rb
+++ app/controllers/event/participation_contact_data/managed_controller.rb
@@ -1,5 +1,16 @@
 
 class Event::ParticipationContactData::ManagedController < Event::ParticipationContactDatasController
+  def update
+    if any_duplicates?
+      entry.errors.add(:base, :duplicates_present) if any_duplicates?
+      render :edit
+    else
+      super
+    end
+  end
+
+  private
+
   def person
     @person ||= Person.new.tap do |person|
       person.managers = [current_user]
@@ -8,5 +19,17 @@ class Event::ParticipationContactData::ManagedController < Event::ParticipationC
 
   def contact_data_class
     Event::ParticipationContactDatas::Managed
+  end
+
+  def any_duplicates?
+    relevant_person_attrs = entry.person.attributes
+                                        .transform_keys(&:to_sym)
+                                        .slice(*Import::PersonDuplicateFinder::DUPLICATE_ATTRIBUTES)
+
+    person_duplicate_finder.find(relevant_person_attrs).present?
+  end
+
+  def person_duplicate_finder
+    @person_duplicate_finder ||= Import::PersonDuplicateFinder.new
   end
 end
