79e69f161cd0af5c2821ff1c42eb0c823295a61c
Nils Rauch <rauch@puzzle.ch>
2023-11-06 11:04:34 +0100

    Use correct repo in license header
    Co-authored-by: Mountain Star <655627+mtnstar@users.noreply.github.com>

--- spec/domain/people/cleanup_finder_spec.rb
+++ spec/domain/people/cleanup_finder_spec.rb
@@ -3,7 +3,7 @@
 #  Copyright (c) 2023, Pfadibewegung Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito.
+#  https://github.com/hitobito/hitobito_youth.
 
 require 'spec_helper'
 
