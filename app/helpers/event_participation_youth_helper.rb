3662ea676e1110de49132a43ed856f67e5e58d72
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-04-04 16:48:46 +0200

    Extract participations show actions into PORO
    refs hitobito/hitobito_sac_cas#369

--- app/helpers/event_participation_youth_helper.rb
+++ app/helpers/event_participation_youth_helper.rb
@@ -1,39 +1,14 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
-#  Copyright (c) 2012-2015, Pfadibewegung Schweiz. This file is part of
+#  Copyright (c) 2012-2024, Pfadibewegung Schweiz. This file is part of
 #  hitobito_youth and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_youth.
 
 module EventParticipationYouthHelper
 
-  def show_event_participation_cancel_button?
-    course_participation_state_transition_allowed?(:cancel, :assigned, :applied)
-  end
-
-  def show_event_participation_reject_button?
-    course_participation_state_transition_allowed?(:reject, :applied)
-  end
-
-  def show_event_participation_absent_button?
-    course_participation_state_transition_allowed?(:absent, :assigned, :attended)
-  end
-
-  def show_event_participation_attended_button?
-    course_participation_state_transition_allowed?(:attend, :absent) && entry.event.closed?
-  end
-
-  def show_event_participation_assigned_button?
-    course_participation_state_transition_allowed?(:assign, :absent) && !entry.event.closed?
-  end
-
   def format_event_participation_state(entry)
     entry.state_translated
   end
 
-  private
-
-  def course_participation_state_transition_allowed?(to_state, *from_states)
-    from_states.collect(&:to_s).include?(entry.state) && can?(to_state, entry)
-  end
 end
