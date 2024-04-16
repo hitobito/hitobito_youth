3b19f4f0a369b4d50cb51007281be1de88cac4f7
Daniel Illi <daniel@illi.zone>
2024-04-16 14:19:58 +0200

    Dropdown::PeopleExport#tabular_links: refactor to match return value of core
    Method now returns item. This allows adding to the menu in subclass.
    
    fixes hitobito/hitobito_sac_cas#427

--- spec/helpers/dropdown/people_export_spec.rb
+++ spec/helpers/dropdown/people_export_spec.rb
@@ -18,87 +18,115 @@ describe 'Dropdown::PeopleExport' do
   let(:entry) { double(event: event) }
   let(:event) { events(:top_course) }
 
-  def can?(*args)
+  def can?(*_args)
     true
+  end
+
+  def top_menu_entries(dropdown)
+    menu = dropdown.find('.btn-group > ul.dropdown-menu')
+    menu.all('> li > a').map(&:text)
+  end
+
+  def submenu_entries(dropdown, name)
+    menu = dropdown.find('.btn-group > ul.dropdown-menu')
+    menu.all("> li > a:contains('#{name}') ~ ul > li > a").map(&:text)
+  end
+
+  it 'renders dropdown' do
+    dropdown = create_dropdown
+    expect(dropdown).to have_content 'Export'
+    expect(dropdown).to have_selector '.btn-group > ul.dropdown-menu'
+
+    expect(top_menu_entries(dropdown)).to match_array %w(CSV Excel vCard PDF)
+
+    expect(submenu_entries(dropdown, 'CSV')).to match_array %w(Spaltenauswahl Adressliste)
   end
 
   context 'nds' do
 
     it 'no nds items if no details permission' do
-      dropdown = create_dropdown.to_s
-      expect(dropdown).to have_content 'Export'
-      expect(dropdown).to have_selector 'a'
-      expect(dropdown).to have_content 'CSV'
-      expect(dropdown).to have_selector 'ul.dropdown-menu > .dropdown >  ul.dropdown-menu.submenu'
-      expect(dropdown).not_to have_content 'NDS-Kurs'
-      expect(dropdown).not_to have_content 'NDS-Lager'
-      expect(dropdown).not_to have_content 'SLRG-Kurs'
+      dropdown = create_dropdown
+      subitems = submenu_entries(dropdown, 'CSV')
+      expect(subitems).not_to include 'NDS-Kurs'
+      expect(subitems).not_to include 'NDS-Lager'
+      expect(subitems).not_to include 'SLRG-Kurs'
     end
 
     it 'no nds items if not people controller' do
-      dropdown = create_dropdown(true, 'people').to_s
-      expect(dropdown).to have_content 'Export'
-      expect(dropdown).to have_selector 'a'
-      expect(dropdown).to have_content 'CSV'
-      expect(dropdown).to have_selector 'ul.dropdown-menu > .dropdown >  ul.dropdown-menu.submenu'
-      expect(dropdown).not_to have_content 'NDS-Kurs'
-      expect(dropdown).not_to have_content 'NDS-Lager'
-      expect(dropdown).not_to have_content 'SLRG-Kurs'
+      dropdown = create_dropdown(true, 'people')
+      subitems = submenu_entries(dropdown, 'CSV')
+      expect(subitems).not_to include 'NDS-Kurs'
+      expect(subitems).not_to include 'NDS-Lager'
+      expect(subitems).not_to include 'SLRG-Kurs'
     end
 
     it 'does add nds items' do
-      dropdown = Capybara.string(create_dropdown(true).to_s)
-      expect(dropdown).to have_content 'Export'
-      expect(dropdown).to have_selector 'ul.dropdown-menu'
-      expect(dropdown).to have_selector 'a'
-      expect(dropdown).to have_content 'CSV'
-      expect(dropdown).to have_selector 'ul.dropdown-menu > .dropdown >  ul.dropdown-menu.submenu'
-      expect(dropdown).to have_content 'NDS-Kurs'
-      expect(dropdown).to have_content 'NDS-Lager'
-      expect(dropdown).to have_content 'SLRG-Kurs'
+      dropdown = create_dropdown(true)
+      subitems = submenu_entries(dropdown, 'CSV')
+      expect(subitems).to include 'NDS-Kurs'
+      expect(subitems).to include 'NDS-Lager'
+      expect(subitems).to include 'SLRG-Kurs'
+    end
+  end
+
+  context 'subclass can override' do
+    def define_subclass(&block)
+      subclass = Class.new(Dropdown::PeopleExport, &block)
+      # Replace the original class with the subclass instead of using it directly,
+      # so I18n will find the original translations.
+      stub_const('Dropdown::PeopleExport', subclass)
     end
 
-    context 'if wagon claims it is a camp' do
-      it 'adds correct NDS items' do
-        allow_any_instance_of(Dropdown::PeopleExport).to receive(:is_camp?).and_return(true)
-        allow_any_instance_of(Dropdown::PeopleExport).to receive(:is_course?).and_return(false)
-        dropdown = Capybara.string(create_dropdown(true).to_s)
-        expect(dropdown).to have_content 'Export'
-        expect(dropdown).to have_selector 'ul.dropdown-menu'
-        expect(dropdown).to have_selector 'a'
-        expect(dropdown).to have_content 'CSV'
-        expect(dropdown).to have_selector 'ul.dropdown-menu > .dropdown >  ul.dropdown-menu.submenu'
-        expect(dropdown).not_to have_content 'NDS-Kurs'
-        expect(dropdown).to have_content 'NDS-Lager'
-        expect(dropdown).not_to have_content 'SLRG-Kurs'
+    it 'if wagon claims it is a camp adds correct NDS items' do
+      define_subclass do
+        def is_camp?(*) = true
+        def is_course?(*) = false
       end
+
+      subitems = submenu_entries(create_dropdown(true), 'CSV')
+      expect(subitems).not_to include 'NDS-Kurs'
+      expect(subitems).to include 'NDS-Lager'
+      expect(subitems).not_to include 'SLRG-Kurs'
     end
 
-    context 'if wagon claims it is a course' do
-      it 'adds correct NDS items' do
-        allow_any_instance_of(Dropdown::PeopleExport).to receive(:is_camp?).and_return(false)
-        allow_any_instance_of(Dropdown::PeopleExport).to receive(:is_course?).and_return(true)
-        dropdown = Capybara.string(create_dropdown(true).to_s)
-        expect(dropdown).to have_content 'Export'
-        expect(dropdown).to have_selector 'ul.dropdown-menu'
-        expect(dropdown).to have_selector 'a'
-        expect(dropdown).to have_content 'CSV'
-        expect(dropdown).to have_selector 'ul.dropdown-menu > .dropdown >  ul.dropdown-menu.submenu'
-        expect(dropdown).to have_content 'NDS-Kurs'
-        expect(dropdown).not_to have_content 'NDS-Lager'
-        expect(dropdown).to have_content 'SLRG-Kurs'
+    it 'if wagon claims it is a course adds correct NDS items' do
+      define_subclass do
+        def is_camp?(*) = false
+        def is_course?(*) = true
       end
+
+      subitems = submenu_entries(create_dropdown(true), 'CSV')
+      expect(subitems).to include 'NDS-Kurs'
+      expect(subitems).not_to include 'NDS-Lager'
+      expect(subitems).to include 'SLRG-Kurs'
+    end
+
+    it '#tabular_links can add subitem' do
+      define_subclass do
+        def tabular_links(format)
+          super.tap do |item|
+            item.sub_items <<
+              Dropdown::Item.new('Hello World', '/hello-world', data: { checkable: true })
+          end
+        end
+      end
+
+      dropdown = create_dropdown
+      expect(top_menu_entries(dropdown)).to match_array %w(CSV Excel vCard PDF)
+      expect(submenu_entries(dropdown,
+                             'CSV')).to match_array ['Spaltenauswahl', 'Adressliste', 'Hello World']
     end
   end
 
   private
+
   def create_dropdown(details = false, controller = 'event/participations')
-    Dropdown::PeopleExport.new(self,
-                               user,
-                               { controller: controller,
-                                 group_id: groups(:top_group).id,
-                                 event_id: events(:top_course).id
-                                },
-                                { details: details })
+    html = Dropdown::PeopleExport.new(self,
+                                      user,
+                                      { controller: controller,
+                                        group_id: groups(:top_group).id,
+                                        event_id: events(:top_course).id },
+                                      { details: details }).to_s
+    Capybara.string(html)
   end
 end
