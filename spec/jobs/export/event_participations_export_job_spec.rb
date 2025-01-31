9d958b549e52e4bfb9e5ec1ab8445c607e21ee05
Daniel Illi <daniel@illi.zone>
2025-01-31 16:51:46 +0100

    Fix spec after method signature change in core (hitobito/hitobito_sac_cas#1512)
    fixes hitobito/hitobito_sac_cas#1512

--- spec/jobs/export/event_participations_export_job_spec.rb
+++ spec/jobs/export/event_participations_export_job_spec.rb
@@ -10,6 +10,7 @@ describe Export::EventParticipationsExportJob do
   subject { Export::EventParticipationsExportJob.new(format,
                                                      user.id,
                                                      event_participation_filter,
+                                                     group.id,
                                                      params.merge(filename: filename)) }
 
   let(:user) { people(:top_leader) }
