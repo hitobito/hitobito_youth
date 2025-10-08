06d97797b28d6001a87e206c6d6e677aa1b89508
Philipp Koster <koster@puzzle.ch>
2025-10-08 13:29:26 +0200

    migrate bulk mail address list

--- lib/hitobito_youth/wagon.rb
+++ lib/hitobito_youth/wagon.rb
@@ -42,7 +42,6 @@ module HitobitoYouth
       Export::Tabular::Events::List.include Youth::Export::Tabular::Events::List
       Export::Tabular::Events::Row.include Youth::Export::Tabular::Events::Row
       Person::AddRequest::Approver::Event.include Youth::Person::AddRequest::Approver::Event
-      Messages::BulkMail::AddressList.include Youth::Messages::BulkMail::AddressList
 
       # ability
       EventAbility.include Youth::EventAbility
