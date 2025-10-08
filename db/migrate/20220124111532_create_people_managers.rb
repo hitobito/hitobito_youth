8a961b5fbeb68273a123dd43b7cbc88bac5910b3
Philipp Koster <koster@puzzle.ch>
2025-10-08 21:07:01 +0200

    only create people managers if not exists

--- db/migrate/20220124111532_create_people_managers.rb
+++ db/migrate/20220124111532_create_people_managers.rb
@@ -8,7 +8,7 @@
 
 class CreatePeopleManagers < ActiveRecord::Migration[6.1]
   def change
-    create_table(:people_managers) do |t|
+    create_table(:people_managers, if_not_exists: true) do |t|
       t.integer :manager_id, null: false
       t.integer :managed_id, null: false
 
