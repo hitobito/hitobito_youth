536159c89fb501b93ccf94d28cf3620016655ab8
Pascal Zumkehr <spam@codez.ch>
2026-05-07 15:57:03 +0200

    Test participations contact data controller for course with open applications (hitobito/hitobito_sac_cas#2398)

--- spec/regressions/event/participations_contact_datas_controller_spec.rb
+++ spec/regressions/event/participations_contact_datas_controller_spec.rb
@@ -1,5 +1,3 @@
-# encoding: utf-8
-
 #  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
@@ -7,79 +5,94 @@
 
 # encoding:  utf-8
 
-require 'spec_helper'
+require "spec_helper"
 
 describe Event::ParticipationContactDatasController, type: :controller do
-
   render_views
 
   let(:group) { groups(:top_layer) }
-  let(:course) { Fabricate(:course, groups: [group]) }
+  let(:course) { Fabricate(:course, groups: [group], state: :application_open) }
   let(:person) { people(:top_leader) }
   let(:dom) { Capybara::Node::Simple.new(response.body) }
 
   before { sign_in(person) }
 
-  describe 'GET edit' do
+  describe "GET edit" do
+    it "does not show hidden contact fields" do
+      get :edit, params: {
+        group_id: course.groups.first.id,
+        event_id: course.id,
+        event_role: {type: "Event::Course::Role::Participant"}
+      }
 
-    it 'does not show hidden contact fields' do
-      get :edit, params: { group_id: course.groups.first.id, event_id: course.id, event_role: { type: 'Event::Course::Role::Participant' } }
-
-      expect(dom).to have_selector('input#event_participation_contact_data_j_s_number')
-      expect(dom).to have_selector('select#event_participation_contact_data_nationality_j_s')
-
-      expect(dom).to have_no_selector('input#event_participation_contact_data_ahv_number')
+      expect(dom).to have_selector("input#event_participation_contact_data_j_s_number")
+      expect(dom).to have_selector("select#event_participation_contact_data_nationality_j_s")
+      expect(dom).to have_no_selector("input#event_participation_contact_data_ahv_number")
     end
 
-    it 'shows all contact fields by default' do
+    it "shows all contact fields by default" do
+      get :edit, params: {
+        group_id: course.groups.first.id,
+        event_id: course.id,
+        event_role: {type: "Event::Course::Role::Participant"}
+      }
 
-      get :edit, params: { group_id: course.groups.first.id, event_id: course.id, event_role: { type: 'Event::Course::Role::Participant' } }
-
-      expect(dom).to have_selector('input#event_participation_contact_data_j_s_number')
-      expect(dom).to have_selector('select#event_participation_contact_data_nationality_j_s')
-
-      expect(dom).to have_no_selector('input#event_participation_contact_data_ahv_number')
+      expect(dom).to have_selector("input#event_participation_contact_data_j_s_number")
+      expect(dom).to have_selector("select#event_participation_contact_data_nationality_j_s")
+      expect(dom).to have_no_selector("input#event_participation_contact_data_ahv_number")
     end
-
   end
 
-  context 'POST update' do
-
+  context "POST update" do
     before do
-      course.update!({ required_contact_attrs: ['j_s_number']})
+      course.update!({required_contact_attrs: ["j_s_number"]})
     end
 
-    it 'validates contact attributes and person attributes' do
+    it "validates contact attributes and person attributes" do
+      contact_data_params = {
+        first_name: "Hans",
+        last_name: "Gugger",
+        email: "hans@youth.com",
+        nickname: "dude",
+        j_s_number: ""
+      }
 
-      contact_data_params = { first_name: 'Hans', last_name: 'Gugger',
-                              email: 'hans@youth.com', nickname: 'dude',
-                              j_s_number: '' }
-
-      post :update, params: { group_id: group.id, event_id: course.id, event_participation_contact_data: contact_data_params, event_role: { type: 'Event::Course::Role::Participant' } }
+      post :update, params: {
+        group_id: group.id,
+        event_id: course.id,
+        event_participation_contact_data: contact_data_params,
+        event_role: {type: "Event::Course::Role::Participant"}
+      }
 
       is_expected.to render_template(:edit)
-
-      expect(dom).to have_selector('.alert-danger li', text: 'J+S-Nummer muss ausgefüllt werden')
-
+      expect(dom).to have_selector(".alert-danger li", text: "J+S-Nummer muss ausgefüllt werden")
     end
 
-    it 'updates person attributes and redirects to event questions' do
+    it "updates person attributes and redirects to event questions" do
+      contact_data_params = {
+        first_name: "Hans",
+        last_name: "Gugger",
+        email: "hans@youth.com",
+        nickname: "dude",
+        j_s_number: "4242"
+      }
 
-      contact_data_params = { first_name: 'Hans', last_name: 'Gugger',
-                              email: 'hans@youth.com', nickname: 'dude',
-                              j_s_number: '4242' }
+      post :update, params: {
+        group_id: group.id,
+        event_id: course.id,
+        event_participation_contact_data: contact_data_params,
+        event_role: {type: "Event::Course::Role::Participant"}
+      }
 
-      post :update, params: { group_id: group.id, event_id: course.id, event_participation_contact_data: contact_data_params, event_role: { type: 'Event::Course::Role::Participant' } }
-
-      is_expected.to redirect_to new_group_event_participation_path(group,
-                                                                    course,
-                                                                    event_role: { type: 'Event::Course::Role::Participant' },
-                                                                    event_participation: { person_id: person.id })
+      is_expected.to redirect_to new_group_event_participation_path(
+        group,
+        course,
+        event_role: {type: "Event::Course::Role::Participant"},
+        event_participation: {person_id: person.id}
+      )
 
       person.reload
-      expect(person.j_s_number).to eq('4242')
-
+      expect(person.j_s_number).to eq("4242")
     end
   end
-
 end
