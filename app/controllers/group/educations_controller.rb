365737a6e6c0489be542e809152613bc47761808
Pascal Zumkehr <zumkehr@puzzle.ch>
2026-09-04 14:46:06 +0200

    Make education list sortable by name, quali and birthday (hitobito/hitobito_sac_cas#2613)

--- app/controllers/group/educations_controller.rb
+++ app/controllers/group/educations_controller.rb
@@ -1,15 +1,58 @@
-#  Copyright (c) 2012-2014, Pfadibewegung Schweiz. This file is part of
+#  Copyright (c) 2012-2026, Pfadibewegung Schweiz. This file is part of
 #  hitobito_youth and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_youth.
 
 class Group::EducationsController < ApplicationController
+  include Sortable
+
   helper_method :group
   decorates :group, :people
 
+  class << self
+    def sort_mappings_with_indifferent_access
+      @sort_mappings_with_indifferent_access ||=
+        {
+          name: Person.order_by_name_statement,
+          qualification: {joins: qualification_order_join,
+                          order: ["sort_qualifications.finish_at",
+                            Person.order_by_name_statement]},
+          birthday: ["people.birthday", Person.order_by_name_statement]
+        }.with_indifferent_access
+    end
+
+    private
+
+    def qualification_order_join
+      "LEFT JOIN (#{earliest_qualification_kind_finish.to_sql}) sort_qualifications " \
+        "ON sort_qualifications.person_id = people.id"
+    end
+
+    # Of all qualification kinds shown for a person, the one expiring first
+    # determines the order.
+    def earliest_qualification_kind_finish
+      Qualification
+        .select("person_id", "MIN(finish_at) AS finish_at")
+        .from(latest_finish_per_qualification_kind, :qualification_kind_finishes)
+        .group("person_id")
+    end
+
+    def latest_finish_per_qualification_kind
+      Qualification
+        .select("qualifications.person_id",
+          "MAX(COALESCE(qualifications.finish_at, 'infinity')) AS finish_at")
+        .joins(:qualification_kind)
+        # same condition as Qualification#reactivateable?
+        .where("qualifications.finish_at IS NULL OR qualifications.finish_at + " \
+               "COALESCE(qualification_kinds.reactivateable, 0) * INTERVAL '1 year' " \
+               ">= CURRENT_DATE")
+        .group("qualifications.person_id", "qualifications.qualification_kind_id")
+    end
+  end
+
   def index
     authorize!(:education, group)
-    @people = education_entries.page(params[:page])
+    @people = list_entries.page(params[:page])
   end
 
   private
@@ -18,15 +61,17 @@ class Group::EducationsController < ApplicationController
     @group ||= Group.find(params[:id])
   end
 
-  def education_entries
-    filter_entries
-      .includes(qualifications: {qualification_kind: :translations},
-        event_participations: {event: [:groups, :dates]})
+  def model_class = Person
+
+  def list_entries
+    person_filter.entries.includes(
+      qualifications: {qualification_kind: :translations},
+      event_participations: {event: [:groups, :dates]}
+    )
   end
 
-  def filter_entries
-    @person_filter = Person::Filter::List.new(group, current_user, list_filter_args)
-    @person_filter.entries.reorder(:birthday)
+  def person_filter
+    @person_filter ||= Person::Filter::List.new(group, current_user, list_filter_args)
   end
 
   def list_filter_args
--- spec/regression/group/educations_controller_spec.rb
+++ spec/regression/group/educations_controller_spec.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-#  Copyright (c) 2012-2015, Pfadibewegung Schweiz. This file is part of
+#  Copyright (c) 2012-2026, Pfadibewegung Schweiz. This file is part of
 #  hitobito_youth and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_youth.
@@ -79,6 +79,94 @@ describe Group::EducationsController, type: :controller do
     expect do
       get :index, params: { id: groups(:top_layer).id, range: :layer, filters: { role: { role_type_ids: Group::TopGroup::Leader.type_id } } }
     end.to raise_error CanCan::AccessDenied
+  end
+
+  describe 'sorting' do
+    let(:group) { groups(:bottom_layer_one) }
+
+    # kind sl has a validity of two, kind gl of one year.
+
+    let!(:alice) do # expires end of next year
+      create_member('Anderegg', 'Alice', Date.new(1990, 3, 1)).tap do |p|
+        create_qualification_for(p, :sl, 1.year.ago.to_date)
+        # of the same kind, only the one expiring last counts
+        create_qualification_for(p, :sl, 2.years.ago.to_date)
+      end
+    end
+
+    let!(:bruno) do # expires end of this year
+      create_member('Berger', 'Bruno', Date.new(1980, 6, 15)).tap do |p|
+        create_qualification_for(p, :sl, Time.zone.today)
+        create_qualification_for(p, :gl, 1.year.ago.to_date)
+      end
+    end
+
+    let!(:carla) do # expires end of next year
+      create_member('Corti', 'Carla', Date.new(1990, 3, 1)).tap do |p|
+        create_qualification_for(p, :sl, 1.year.ago.to_date)
+        # already expired and thus not shown
+        create_qualification_for(p, :gl, 3.years.ago.to_date)
+      end
+    end
+
+    def create_member(last_name, first_name, birthday)
+      Fabricate(Group::BottomLayer::Member.name.to_sym, group: group).person.tap do |p|
+        p.update!(last_name: last_name, first_name: first_name, birthday: birthday)
+      end
+    end
+
+    def create_qualification_for(person, kind, start_at)
+      Qualification.create!(person: person,
+                            qualification_kind: qualification_kinds(kind),
+                            start_at: start_at)
+    end
+
+    def sorted_ids(params = {})
+      get :index, params: { id: group.id }.merge(params)
+      assigns(:people).collect(&:id) & [alice.id, bruno.id, carla.id]
+    end
+
+    it 'sorts by person name by default' do
+      expect(sorted_ids).to eq [alice.id, bruno.id, carla.id]
+    end
+
+    it 'sorts by person name descending' do
+      expect(sorted_ids(sort: :name, sort_dir: :desc)).to eq [carla.id, bruno.id, alice.id]
+    end
+
+    it 'sorts by the first expiring qualification kind, then by person name' do
+      expect(sorted_ids(sort: :qualification, sort_dir: :asc)).to eq [bruno.id, alice.id, carla.id]
+    end
+
+    it 'sorts never expiring qualifications last' do
+      dora = create_member('Durrer', 'Dora', Date.new(1990, 3, 1))
+      Qualification.create!(person: dora, start_at: 1.year.ago.to_date,
+                            qualification_kind: Fabricate(:qualification_kind, validity: nil))
+      # no quali at all
+      elodie = create_member('Eberhard', 'Elodie', Date.new(1985, 6, 15))
+      get :index, params: { id: group.id, sort: :qualification, sort_dir: :asc }
+      ids = assigns(:people).collect(&:id) & [alice.id, bruno.id, carla.id, dora.id, elodie.id]
+      expect(ids).to eq [bruno.id, alice.id, carla.id, dora.id, elodie.id]
+    end
+
+    it 'sorts all people below a group by qualification descending' do
+      get :index, params: { id: groups(:top_layer).id, range: :deep,
+                            sort: :qualification, sort_dir: :desc }
+      ids = assigns(:people).collect(&:id) & [alice.id, bruno.id, carla.id]
+      # the second order criteria follows the sort direction of the first one
+      expect(ids).to eq [carla.id, alice.id, bruno.id]
+    end
+
+    it 'sorts by birthday, then by person name' do
+      expect(sorted_ids(sort: :birthday, sort_dir: :asc)).to eq [bruno.id, alice.id, carla.id]
+    end
+
+    it 'renders sort links for the sortable columns' do
+      get :index, params: { id: group.id }
+      expect(response.body).to have_css("th a[href*='sort=name']")
+      expect(response.body).to have_css("th a[href*='sort=qualification']")
+      expect(response.body).to have_css("th a[href*='sort=birthday']")
+    end
   end
 
 end
